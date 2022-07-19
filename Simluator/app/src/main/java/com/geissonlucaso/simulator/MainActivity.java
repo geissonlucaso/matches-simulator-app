460e983c2231fcdd5fa8c6dc9f879eb41a3bf0e5
Geisson de Oliveira <geissonlucaso@gmail.com>
2022-07-19 12:28:48 -0300

    Applying ViewBinding and SwipeRefreshLayout.

---
+++ Simluator/app/src/main/java/com/geissonlucaso/simulator/MainActivity.java
@@ -0,0 +1,22 @@
+package com.geissonlucaso.simulator;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.geissonlucaso.simulator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.tvHello.setText("Geisson");
+    }
+}
