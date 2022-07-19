e9b88dd63690ec6a96e7de89edb2c1fd6b712a8b
Geisson de Oliveira <geissonlucaso@gmail.com>
2022-07-18 23:48:39 -0300

    First commit.

---
+++ Simluator/app/src/main/java/com/geissonlucaso/simulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.geissonlucaso.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
