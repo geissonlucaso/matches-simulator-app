d8fdbf37330230e85a1f89c876744d5d363d1290
Geisson de Oliveira <geissonlucaso@gmail.com>
2022-07-21 15:54:06 -0300

    Creating domain classes.

---
+++ Simluator/app/src/main/java/com/geissonlucaso/simulator/domain/Place.java
@@ -0,0 +1,22 @@
+package com.geissonlucaso.simulator.domain;
+
+public class Place {
+    private String name;
+    private String picture;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
---
+++ Simluator/app/src/main/java/com/geissonlucaso/simulator/domain/Team.kt
@@ -0,0 +1,7 @@
+package com.geissonlucaso.simulator.domain
+
+data class Team(
+    val name: String,
+    val power: Int,
+    val picture: String
+)
