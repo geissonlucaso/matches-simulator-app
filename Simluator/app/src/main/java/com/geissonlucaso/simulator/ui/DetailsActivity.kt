2b905012a2e5da8712012513a2b6081070623fbb
Geisson de Oliveira <geissonlucaso@gmail.com>
2022-07-25 16:27:14 -0300

    Passing data to Details page.

--- Simluator/app/src/main/java/com/geissonlucaso/simulator/ui/DetailsActivity.kt
+++ Simluator/app/src/main/java/com/geissonlucaso/simulator/ui/DetailsActivity.kt
@@ -2,8 +2,10 @@ package com.geissonlucaso.simulator.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.geissonlucaso.simulator.databinding.ActivityDetailsBinding
 import com.geissonlucaso.simulator.databinding.ActivityMainBinding
+import com.geissonlucaso.simulator.domain.Match
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -21,5 +23,29 @@ class DetailsActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        loadMatchFromExtra()
+    }
+
+    private fun loadMatchFromExtra() {
+        intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let {
+            Glide.with(this).load(it.place.picture).into(binding.ivPlace)
+            supportActionBar?.title = it.place.name
+
+            binding.tvDescription.text = it.description
+
+            Glide.with(this).load(it.homeTeam.picture).into(binding.ivHomeTeam)
+            binding.tvHomeTeamName.text = it.homeTeam.name
+            binding.rbHomeTeamStars.rating = it.homeTeam.power.toFloat()
+            if (it.homeTeam.score != null)
+                binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
+
+            Glide.with(this).load(it.awayTeam.picture).into(binding.ivAwayTeam)
+            binding.tvAwayTeamName.text = it.awayTeam.name
+            binding.rbAwayTeamStars.rating = it.awayTeam.power.toFloat()
+            if (it.awayTeam.score != null)
+                binding.tvAwayTeamScore.text = it.awayTeam.score.toString()
+
+        }
     }
 }
